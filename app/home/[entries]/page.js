2a894386b0254cc5db7f4e1f1e8c9ba8821f42e5
Tawfiq <t.mouinet@gmail.com>
2023-10-21 14:57:00 +0100

    Created tables, connected database, and added constraints and filtering.

---
+++ app/home/[entries]/page.js
@@ -0,0 +1,28 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export default async function Entry({ params }) {
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+  const topic_id = params.entries;
+  const { data: entries } = await supabase
+    .from("entry")
+    .select()
+    .eq("topic", topic_id);
+  return (
+    <main>
+      <ul>
+        {entries?.map((entry) => (
+          <li>
+            <h1 className="text-xl mb-4">{entry.title}</h1>
+            <p>
+              {entry.text.length > 200
+                ? entry.text.substring(0, 200).concat("...")
+                : entry.text}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
--- app/home/page.js
+++ app/home/page.js
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <main></main>;
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import Link from "next/link";
+
+export default async function Home() {
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+  const { data: topics } = await supabase.from("topic").select();
+  return (
+    <main>
+      <table className="m-auto border-collapse border border-slate-300 table-auto text-white ">
+        <tr className="border border-slate-300">
+          <th className="border border-slate-300">Topic</th>
+          <th className="border border-slate-300">Entries</th>
+          <th className="border  border-slate-300">Date</th>
+        </tr>
+        {topics?.map((topic) => (
+          <tr key={topic.id} className="border border-slate-300">
+            <td className="border border-slate-300 text-center">
+              <Link href={`/home/${topic.id}`} prefetch={false}>
+                {topic.title}
+              </Link>
+            </td>
+            <td className="border border-slate-300 text-center">
+              {topic.number_of_entries}
+            </td>
+            <td className="border border-slate-300 text-center">
+              {topic.created_at.substring(0, 10)}
+            </td>
+          </tr>
+        ))}
+      </table>
+    </main>
+  );
 }
