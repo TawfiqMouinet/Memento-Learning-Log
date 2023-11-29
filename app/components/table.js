2c6a77ef59200fdb62a3a46d211fe6eada731dbf
Tawfiq <t.mouinet@gmail.com>
2023-11-29 10:17:25 +0100

    Apah

---
+++ app/components/table.js
@@ -0,0 +1,29 @@
+"use client";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/table";
+
+export default function NewTable({ rows, columns }) {
+  return (
+    <Table aria-label="Table of topics">
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody items={rows}>
+        {(item) => (
+          <TableRow key={item.key}>
+            {(columnKey) => (
+              <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
--- app/home/page.js
+++ app/home/page.js
@@ -2,43 +2,43 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
+import NewTable from "../components/table";
 import NewModal from "../components/modal";
 import { Button } from "@nextui-org/button";
 import NewTopic from "../components/new_topic";
 
 export default async function Home() {
+  "use server";
   const cookieStore = cookies();
-  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+  const supabase = createServerComponentClient({
+    cookies: () => cookieStore,
+  });
   const { data: topics } = await supabase.from("topic").select();
+  const rows = topics?.map((topic) => ({
+    key: topic.id,
+    title: topic.title,
+    num_of_entries: topic.number_of_entries,
+    created_at: topic.created_at.substring(0, 10),
+  }));
+
+  const columns = [
+    {
+      key: "Topic",
+      label: "TOPIC",
+    },
+    {
+      key: "Entries",
+      label: "ENTRIES",
+    },
+    {
+      key: "Date",
+      label: "DATE",
+    },
+  ];
 
   return (
     <main>
-      <table className="m-auto border-collapse border border-slate-300 table-auto text-white ">
-        <thead>
-          <tr className="border border-slate-300">
-            <th className="border border-slate-300">Topic</th>
-            <th className="border border-slate-300">Entries</th>
-            <th className="border  border-slate-300">Date</th>
-          </tr>
-        </thead>
-        {topics?.map((topic) => (
-          <tbody>
-            <tr key={topic.id} className="border border-slate-300">
-              <td className="border border-slate-300 text-center">
-                <Link href={`/home/${topic.id}`} prefetch={false}>
-                  {topic.title}
-                </Link>
-              </td>
-              <td className="border border-slate-300 text-center">
-                {topic.number_of_entries}
-              </td>
-              <td className="border border-slate-300 text-center">
-                {topic.created_at.substring(0, 10)}
-              </td>
-            </tr>
-          </tbody>
-        ))}
-      </table>
+      <NewTable rows={rows} columns={columns} />
       <NewModal
         buttonText={"Add Topic"}
         modalHeader={"Create a new Topic"}
